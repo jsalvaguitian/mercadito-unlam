a670332176b04a14ef37ad777cfb4a09b85f29b9
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2024-07-27 06:48:15 -0300

    se arregló el estilo de las vista registro de usuario

---
+++ js/configuracion-localStorage-BBDD.js
@@ -0,0 +1,11 @@
+const Categorias = [
+    Object.freeze({id:1, nombre: 'OFERTAS', imagen: ''}),
+    Object.freeze({id:2, nombre: 'ALMACEN', imagen: ''}),
+    Object.freeze({id:3, nombre: 'BEBIDAS',imagen: ''}),
+    Object.freeze({id:4, nombre: 'CONGELADOS', imagen: ''}),
+    Object.freeze({id:5, nombre: 'ELECTRODOMÉSTICOS', imagen: ''}),
+    Object.freeze({id:6, nombre: 'HOGAR Y TEXTIL', imagen: ''}),
+    Object.freeze({id:7, nombre: 'LACTEOS', imagen: ''}),
+    Object.freeze({id:8, nombre: 'TECNOLOGIA', imagen: ''}),
+    Object.freeze({id:9, nombre: 'FRUTAS Y VERDURAS', imagen: ''}),
+]
